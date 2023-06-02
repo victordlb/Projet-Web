cb39ef9b23a77368dca060d89ea17b9ad35e7133
PaulNougarede <paul.nougarede78@gmail.com>
2023-06-02 17:02:13 +0200

    ajout notif

--- Projet Web/php/notifications.php
+++ Projet Web/php/notifications.php
@@ -21,7 +21,6 @@ while($auser = mysqli_fetch_assoc($data))
 <body>
     <h1>Vos Notifications</h1>
     <a href=../php/connexion.php>Retourner a la page de connexion</a>
-
     <nav class="navbar">
         <ul>
             <li><a href="../php/accueil.php">Accueil</a></li>
@@ -40,5 +39,59 @@ while($auser = mysqli_fetch_assoc($data))
             <li><a href="../php/compte.php">Votre Compte</a></li>
         </ul>
     </nav>
+    <?php
+
+        $database = "piscine";
+        $db_handle = mysqli_connect('db', 'root', 'mypassword', $database);
+        $db_found = mysqli_select_db($db_handle, $database);
+
+        if(!$db_handle)
+        {
+                echo "Connexion db echoue";
+        }
+        elseif($vendeur)
+        {
+            $error = "";
+            $requete = "SELECT * FROM notif";
+            if ($error)
+            {
+                // S'il y a eu une erreur, on l'affiche
+                echo "<p>Error: $error</p>";
+            } 
+            else 
+            {
+                echo "<div id='container'>";
+                $results = mysqli_query($db_handle, $requete);
+                if (!$results || mysqli_num_rows($results) == 0) {
+                    echo "<h1>Pas de Notifications</h1>";
+                } else {
+                    echo '
+                        <table>
+                            <tr>
+                                <th>Type</th>
+                                <th>Message</th>
+                                <th>Date</th>
+                                <th>Acheteur</th>
+                            </tr>
+                    ';
+                    //afficher le resultat
+                    while ($data = mysqli_fetch_assoc($results)) {
+                        echo "<tr>";
+                        echo "<td>" . $data['type'] . "</td>";
+                        echo "<td>" . $data['message'] . "</td>";
+                        echo "<td>" . $data['date'] . "$</td>";
+                        echo "<td>" . $data['ID_user'] . "</td>";
+                    }
+                    echo "</table>";
+                    echo "</div>";
+                }
+            }
+            mysqli_close($db_handle);
+        }elseif(!$vendeur){
+            echo "<li><a href='../php/nvlnotif.php'>Ajouter une notification</a></li>";
+        }
+
+    ?>
+
 </body>
 </html>
